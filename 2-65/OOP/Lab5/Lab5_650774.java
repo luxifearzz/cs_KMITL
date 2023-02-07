0ebe111e9fb636c9cc54ab910d48d94ae18e4049
luxifearzz <luxifearzz@gmail.com>
2023-02-07 12:13:04 +0700

    Add Lab5 element

---
+++ 2-65/OOP/Lab5/Lab5_650774.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class Lab5_650774 {
+    static int[] data = { 28, 58, 8, 77, 48, 39 };
+    static ArrayProcessor q1NumberOfEvenElement; // 4
+    static ArrayProcessor q2IndexOfLargestEvenValue; // 1
+    static ArrayProcessor myMedian; // n/2th element of sorted = 48
+    
+    public static void main(String[] args) {
+        q1(); // 4
+        q2(); // 1
+        oneline(); // 48
+    }
+    
+    static void q1() {
+        q1NumberOfEvenElement = arr -> {
+            int cnt=0;
+            for(int n : arr) cnt += n%2==0 ? 1 : 0;
+            return cnt;
+        };
+        System.out.println(q1NumberOfEvenElement.calculate(data));
+    }
+
+    static void q2() {
+        q2IndexOfLargestEvenValue = arr -> {
+            int max = Integer.MIN_VALUE, idx = -1;
+            for(int i=0; i<arr.length; i++) {
+                if (arr[i] % 2 == 0 && arr[i] > max) {
+                    max = arr[i];
+                    idx = i;
+                }
+            }
+            return idx;
+        };
+        System.out.println(q2IndexOfLargestEvenValue.calculate(data));
+    }
+
+    static void oneline() {
+        int[] tmp = Arrays.copyOf(data, data.length);
+        Arrays.sort(tmp);
+        myMedian = arr -> {return arr[arr.length/2];};
+        System.out.println(myMedian.calculate(tmp)); // 77 ? this output is 48 from line 12
+    }
+}
