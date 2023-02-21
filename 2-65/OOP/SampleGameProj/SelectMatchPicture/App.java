7a4f42af5c0f6e93ff83a4f76922a13c4b357580
luxifearzz <luxifearzz@gmail.com>
2023-02-21 22:42:57 +0700

    finished develop App.java and finished note.txt

--- 2-65/OOP/SampleGameProj/SelectMatchPicture/App.java
+++ 2-65/OOP/SampleGameProj/SelectMatchPicture/App.java
@@ -4,8 +4,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
@@ -13,6 +13,8 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.Timer;
+import javax.swing.JOptionPane;
 
 public class App implements ActionListener {
     JFrame f;
@@ -41,6 +43,17 @@ public class App implements ActionListener {
     JLabel lTimer;
     JButton bReset;
     JButton bNext;
+    ImageIcon iAns1;
+    ImageIcon iAns2;
+    ImageIcon iAns3;
+    ImageIcon iAns4;
+    ImageIcon iUser1;
+    ImageIcon iUser2;
+    ImageIcon iUser3;
+    ImageIcon iUser4;
+
+    Timer timer1;
+    Timer timer2;
 
     boolean g1isEmpty = true;
     boolean g2isEmpty = true;
@@ -49,7 +62,10 @@ public class App implements ActionListener {
 
     boolean isStart = false;
 
+    boolean isFreeze = false;
+
     int score;
+    int time_limit;
 
     // initialize fruits icon
     ImageIcon close = new ImageIcon("SelectMatchPicture/element/close.png");
@@ -75,6 +91,8 @@ public class App implements ActionListener {
     }
 
     private void start() {
+        time_limit = 15;
+
         lHint1.setIcon(close);
         lHint2.setIcon(close);
         lHint3.setIcon(close);
@@ -89,11 +107,19 @@ public class App implements ActionListener {
         g2isEmpty = true;
         g3isEmpty = true;
         g4isEmpty = true;
+
+        iAns1 = null;
+        iAns2 = null;
+        iAns3 = null;
+        iAns4 = null;
+        iUser1 = null;
+        iUser2 = null;
+        iUser3 = null;
+        iUser4 = null;
         
         isStart = false;
 
-        score = 0;
-        lScore.setText("Your score : " + score);
+        isFreeze = false;
     }
 
     private void initialize() {
@@ -101,6 +127,7 @@ public class App implements ActionListener {
         initializeSelectPanel();
         initializeGamePanel();
         initializeTimerPanel();
+        start();
     }
 
     private void initializeTimerPanel() {
@@ -124,7 +151,7 @@ public class App implements ActionListener {
 
         pTools = new JPanel();
         pTools.setBackground(Color.lightGray);
-        pTools.setBounds(360, 480, 400, 100);
+        pTools.setBounds(360, 480, 400, 100);   // begin at 480px top, 360px left with 400px width, 100px height.
         pTools.setLayout(new FlowLayout());
 
         pTools.add(lScore);
@@ -147,15 +174,9 @@ public class App implements ActionListener {
 
         pGame = new JPanel();
         pGame.setBackground(Color.gray);
-        pGame.setBounds(360, 20, 400, 400);
-        pGame.setLayout(new GridLayout(2, 2, 25, 25));
-        pGame.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-
-        // * set icon here * //
-        bGame1.setIcon(empty);
-        bGame2.setIcon(empty);
-        bGame3.setIcon(empty);
-        bGame4.setIcon(empty);
+        pGame.setBounds(360, 20, 400, 400); // begin at 20px top, 360px left with 400px width, 400px height.
+        pGame.setLayout(new GridLayout(2, 2, 25, 25));  // create gridlayout size 2*2 that has space 25px.
+        pGame.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));   // set margin around 10px.
 
         pGame.add(bGame1);
         pGame.add(bGame2);
@@ -188,7 +209,7 @@ public class App implements ActionListener {
         pSelect = new JPanel();
         pSelect.setBackground(Color.gray);
         pSelect.setBounds(20, 360, 280, 280);   // begin at 360px top, 20px left with 280px width, 280px height.
-        pSelect.setLayout(new GridLayout(3, 3, 6, 6));  // create gridlayout size 3*3 that has space 6px
+        pSelect.setLayout(new GridLayout(3, 3, 6, 6));  // create gridlayout size 3*3 that has space 6px.
         pSelect.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // set margin around 10px.
 
         bSel1.setIcon(f1);
@@ -223,14 +244,8 @@ public class App implements ActionListener {
         pHint = new JPanel();
         pHint.setBackground(Color.gray);
         pHint.setBounds(20, 20, 280, 280);  // begin at 20px top, 20px left with 280px width, 280px height.
-        pHint.setLayout(new GridLayout(2, 2, 10, 10));  // create gridlayout size 2*2 that has space 10px
+        pHint.setLayout(new GridLayout(2, 2, 10, 10));  // create gridlayout size 2*2 that has space 10px.
         pHint.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));   // set margin around 10px.
-        
-        // * random icon here* //
-        lHint1.setIcon(close);
-        lHint2.setIcon(close);
-        lHint3.setIcon(close);
-        lHint4.setIcon(close);
 
         pHint.add(lHint1);
         pHint.add(lHint2);
@@ -239,72 +254,151 @@ public class App implements ActionListener {
 
         f.add(pHint);
     }
-
-    private void setTimer() {
-        for (int i = 10; i >= 0; i--) {
-            lTimer.setText("Time Left : " + i);
-            try {
-                Thread.sleep(1000);
-            } catch (Exception e) {
-                System.out.println(e);
-            }
-        }
-    }
-
+    
     public void actionPerformed(ActionEvent ev) {
         JButton src = (JButton)ev.getSource();
-        if (src == bSel1 || src == bSel2 || src == bSel3 || src == bSel4 || src == bSel5 || src == bSel6 || src == bSel7 || src == bSel8 || src == bSel9) {
+
+        if (!isFreeze && (src == bSel1 || src == bSel2 || src == bSel3 || src == bSel4 || src == bSel5 || src == bSel6 || src == bSel7 || src == bSel8 || src == bSel9)) {
             if (!isStart) return;
             addToGame(src);
             bNext.setText("Submit");
-        } else if (src == bGame1 || src == bGame2 || src == bGame3 || src == bGame4) {
+
+        } else if (!isFreeze && (src == bGame1 || src == bGame2 || src == bGame3 || src == bGame4)) {
             if (!isStart) return;
             removeFromGame(src);
-        } else if (src == bReset) {
+
+        } else if (src == bReset) { // back to prepare stage.
             start();
+            time_limit = 0;
+            score = 0;
+            lScore.setText("Your score : " + score);
             bNext.setText("Start");
             isStart = false;
+
         } else if (src == bNext) {
-            if (isStart) {
-                checkAns();
+            if (isStart) {  // button is show as submit.
+                if (checkAns()) {
+                    lScore.setText("Your score : "+ ++score);
+                    JOptionPane.showMessageDialog(f, "Congrats!");
+                } else {
+                    JOptionPane.showMessageDialog(f, "Sorry, Wrong answer.");
+                }
+                
+                timer2.stop();
+
                 bNext.setText("Next");
                 isStart = false;
-            } else {
+
+            } else {    // button is show as start or next.
                 start();
+
                 bNext.setText("Submit");
                 isStart = true;
+
+                openCard();
+
             }
         }
     }
 
-    private void removeFromGame(JButton btn) {
-        if (btn == bGame1) g1isEmpty = true;
-        if (btn == bGame2) g2isEmpty = true;
-        if (btn == bGame3) g3isEmpty = true;
-        if (btn == bGame4) g4isEmpty = true;
-        btn.setIcon(empty);
+    private void openCard() {
+        ImageIcon rand1 = randomFruit();
+        iAns1 = rand1;
+        lHint1.setIcon(rand1);
+        ImageIcon rand2 = randomFruit();
+        iAns2 = rand2;
+        lHint2.setIcon(rand2);
+        ImageIcon rand3 = randomFruit();
+        iAns3 = rand3;
+        lHint3.setIcon(rand3);
+        ImageIcon rand4 = randomFruit();
+        iAns4 = rand4;
+        lHint4.setIcon(rand4);
+
+        isFreeze = true;
+        timer1 = new Timer(5000, e -> {
+            lHint1.setIcon(close);
+            lHint2.setIcon(close);
+            lHint3.setIcon(close);
+            lHint4.setIcon(close);
+            isFreeze = false;
+            gameTimer();
+        });
+        timer1.setRepeats(false);
+        timer1.start();
     }
 
+    private void gameTimer() {
+        timer2 = new Timer(1000, e -> {
+            lTimer.setText("Time Left : " + time_limit--);
+            if (time_limit < 0) {
+                timer2.stop();
+                lTimer.setText("Time out!!");
+                isFreeze = true;
+            }
+        });
+        timer2.start();
+    }
+    
+    private void removeFromGame(JButton btn) {
+        if (btn == bGame1) {
+            g1isEmpty = true;
+            iUser1 = null;
+        } else if (btn == bGame2) {
+            g2isEmpty = true;
+            iUser2 = null;
+        } else if (btn == bGame3) {
+            g3isEmpty = true;
+            iUser3 = null;
+        } else if (btn == bGame4) {
+            g4isEmpty = true;
+            iUser4 = null;
+        }
+        btn.setIcon(empty);
+    }
+    
     private void addToGame(JButton btn) {
+        ImageIcon icon = evalIcon(btn);
         if (g1isEmpty) {
-            bGame1.setIcon(evalIcon(btn));
+            iUser1 = icon;
+            bGame1.setIcon(icon);
             g1isEmpty = false;
         } else if (g2isEmpty) {
-            bGame2.setIcon(evalIcon(btn));
+            iUser2 = icon;
+            bGame2.setIcon(icon);
             g2isEmpty = false;
         } else if (g3isEmpty) {
-            bGame3.setIcon(evalIcon(btn));
+            iUser3 = icon;
+            bGame3.setIcon(icon);
             g3isEmpty = false;
         } else if (g4isEmpty) {
-            bGame4.setIcon(evalIcon(btn));
+            iUser4 = icon;
+            bGame4.setIcon(icon);
             g4isEmpty = false;
         }
     }
-
-    private void checkAns() {
-
+    
+    private boolean checkAns() {
+        if (iUser1 == iAns1 && iUser2 == iAns2 && iUser3 == iAns3 && iUser4 == iAns4) return true;
+        return false;
     }
-
+    
+    private ImageIcon randomFruit() {
+        int rnd = (int)(Math.random() * 10) % 9;
+        switch (rnd) {
+            case 0 : return f1;
+            case 1 : return f2;
+            case 2 : return f3;
+            case 3 : return f4;
+            case 4 : return f5;
+            case 5 : return f6;
+            case 6 : return f7;
+            case 7 : return f8;
+            case 8 : return f9;
+            default : return new ImageIcon();
+        }
+    }
+    
     private ImageIcon evalIcon(JButton btn) {
         if (btn == bSel1) return f1;
         if (btn == bSel2) return f2;
